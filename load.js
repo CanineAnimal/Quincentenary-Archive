3615cd3b7d7484eb5fe807c8e1387f854f9d3e7e
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-08-29 18:15:18 +1200

    Update load.js

--- load.js
+++ load.js
@@ -3,6 +3,7 @@ function accessArchive(num, ext){
 		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
 	}else{
 		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://cdn.jsdelivr.net/gh/CanineAnimal/QA-Archives@main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
+	}
 	document.querySelector('.LINKS').innerHTML = '<A HREF="https://canineanimal.github.io/Quincentenary-Archive/">Back</A> | <A HREF="#" ONCLICK="closeIframe()">Close archive</A>';
 }
 function closeIframe(){
