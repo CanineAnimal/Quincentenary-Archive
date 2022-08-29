c0e8ea851d7de4134f95c940baf50c94b42e0d39
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-08-30 11:32:21 +1200

    Updated quincentenaryarchive.json

--- src/jsongenerator.js
+++ src/jsongenerator.js
@@ -1,6 +1,4 @@
 // This should be ran on any HTML archive version of a page on a thread. Nb generated code should be appended to existing JSON archive prior to the closing } bracket.
-var months = [,'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-var pmify = ['am',,,,,,,,,,,,'pm'];
 var pages = eval(prompt('How many pages does this thread have?'));
 var parser = new DOMParser();
 var request = new XMLHttpRequest();
@@ -12,13 +10,6 @@ if(parser.parseFromString(request.responseText, 'text/html').querySelectorAll('.
 }else{
 	json.lock = true;
 }
-function twoDigitify(num){
-	if(num.length == 1){
-		return '0' + num.toString();
-	}else{
-		return num.toString();
-	}
-}
 json.title = document.querySelector('#page-body h2 a').innerText;
 json.posts = [];
 threadID = document.querySelectorAll('A')[12].attributes.href.value.split('=')[2];
@@ -29,12 +20,10 @@ for(var item = 0; item < pages; item++){
 	if(request.status == 200){
 		posts = parser.parseFromString(request.responseText, 'text/html').querySelectorAll('.post');
 		for(var jtem = 0; jtem < posts.length; jtem++){
-			rd = posts[jtem].querySelector('.author').innerText.split('» ')[1].split(' ');
 			json.posts[json.posts.length] = {
 				posterID: posts[jtem].querySelectorAll('A')[1].attributes.href.value.split('=')[2],
 				posterName: posts[jtem].querySelector('.author').querySelector('Strong a').innerHTML,
 				postID: posts[jtem].querySelector('.author').querySelector('a').href.split('#')[1],
-				timestamp: new Date(rd[3] + '-' + twoDigitify(months.indexOf(rd[1])) + '-' + rd[2].split(',')[0] + 'T' + twoDigitify(eval(rd[4].split(':')[0]) + pmify.indexOf(rd[5])) + ':' + twoDigitify(rd[4].split(':')[1]) + ':00.000-07:00'),
 				postHTML: posts[jtem].querySelector('.content').innerHTML
 			}
 		}
