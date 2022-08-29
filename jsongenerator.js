585d582c21e106d09514387a9ca5b39a4fe24e11
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-08-30 07:39:02 +1200

    Update jsongenerator.js

--- jsongenerator.js
+++ jsongenerator.js
@@ -21,7 +21,7 @@ for(var item = 0; item < pages; item++){
 			json.posts[json.posts.length] = {
 				posterID: posts[jtem].querySelectorAll('A')[1].attributes.href.value.split('=')[2],
 				posterName: posts[jtem].querySelector('.author').querySelector('Strong a').innerHTML,
-				timestamp: posts[jtem].querySelector('.author').innerText.split('» ')[1] + '",',
+				timestamp: posts[jtem].querySelector('.author').innerText.split('» ')[1],
 				postHTML: posts[jtem].querySelector('.content').innerHTML
 			}
 		}
