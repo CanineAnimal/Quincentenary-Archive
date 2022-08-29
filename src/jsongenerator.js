7ae3e930aec75df96e4f43c70f3b5b3d688ed3f2
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-08-30 09:01:13 +1200

    Organising with directories etc

---
+++ src/jsongenerator.js
@@ -0,0 +1,36 @@
+// This should be ran on any HTML archive version of a page on a thread. Nb generated code should be appended to existing JSON archive prior to the closing } bracket.
+var pages = eval(prompt('How many pages does this thread have?'));
+var parser = new DOMParser();
+var request = new XMLHttpRequest();
+request.open('GET', '0.html', false);
+request.send();
+json = {}
+if(parser.parseFromString(request.responseText, 'text/html').querySelectorAll('.locked-icon').length == 0){
+	json.lock = false;
+}else{
+	json.lock = true;
+}
+json.title = document.querySelector('#page-body h2 a').innerText;
+json.posts = [];
+threadID = document.querySelectorAll('A')[12].attributes.href.value.split('=')[2];
+for(var item = 0; item < pages; item++){
+	var request = new XMLHttpRequest();
+	request.open('GET', item + '.html', false);
+	request.send();
+	if(request.status == 200){
+		posts = parser.parseFromString(request.responseText, 'text/html').querySelectorAll('.post');
+		for(var jtem = 0; jtem < posts.length; jtem++){
+			json.posts[json.posts.length] = {
+				posterID: posts[jtem].querySelectorAll('A')[1].attributes.href.value.split('=')[2],
+				posterName: posts[jtem].querySelector('.author').querySelector('Strong a').innerHTML,
+				postID: posts[jtem].querySelector('.author').querySelector('a').href.split('#')[1],
+				timestamp: posts[jtem].querySelector('.author').innerText.split('» ')[1],
+				postHTML: posts[jtem].querySelector('.content').innerHTML
+			}
+		}
+	}else{
+		json.posts[json.posts.length] = null;
+	}
+}
+document.body.innerHTML = '<TEXTAREA></TEXTAREA>';
+document.querySelector('TEXTAREA').innerText = '"t' + threadID + '":' + JSON.stringify(json) + ',';
---
+++ src/save.py
@@ -0,0 +1,60 @@
+# Dependencies: PdfKit, WkHtmlToPdf, GitPython
+# https://pypi.org/project/pdfkit/
+# https://wkhtmltopdf.org/
+# https://gitpython.readthedocs.io/en/stable/
+# Nb: this code is run in a clone of the QA-Archive repo.
+
+# Get modules
+import requests
+import github
+import pdfkit
+import time
+import git
+
+
+# Get inputs
+threadLink = input('Enter thread link: ')
+pageNo = int(input('Enter the amount of pages: '))
+tn = input('Enter shorthand version of thread name: ')
+repo = git.Repo('.git')
+
+# Loop through thread
+item = 0
+while item < pageNo:
+	try:
+		# Save page HTML
+		print('Fetching ' + str(item ) + '.html...')
+		page = threadLink + '&start=' + str(item * 25)
+		html = requests.get(page, headers={'User-Agent': 'Script by The Ice States to save a Forum 7 thread.'}).text.replace('href="./', 'href="https://forum.nationstates.net/').replace('src="./', 'href="https://forum.nationstates.net/').replace('src="//', 'src="https://').replace('href="//', 'href="https://')
+		print('Saving ' + str(item ) + '.html...')
+		file = open(tn + '/' + str(item) + '.html', 'w')
+		file.write(html)
+		file.close()
+		time.sleep(1)
+		print('Adding ' + str(item) + '.html...')
+		repo.index.add([folder + '/' + str(item) + '.html'])
+		print('Committing changes...')
+		repo.index.commit('Upload HTML Page')
+		print('Pushing changes...')
+		origin = repo.remote(name='origin')
+		origin.push()
+		print('Uploaded page ' + str(item + 1) + ' in HTML!')
+		time.sleep(1)
+
+		# Save page PDF
+		print('Saving ' + str(item) + '.pdf...')
+		pdfkit.from_url(page, str(item) + '.pdf', options={'custom-header':[('User-Agent','Script by The Ice States to save a Forum 7 thread.')]})
+		time.sleep(1)
+		print('Adding ' + str(item) + '.pdf...')
+		repo.index.add([folder + '/' + str(item) + '.pdf'])
+		print('Committing changes...')
+		repo.index.commit('Upload PDF Page')
+		print('Pushing changes...')
+		origin = repo.remote(name='origin')
+		origin.push()
+		print('Completed!')
+		time.sleep(1)
+		print('Uploaded page ' + str(item + 1) + ' in PDF!')
+		item += 1
+	except:
+		print('Unable to save page ' + str(item))
