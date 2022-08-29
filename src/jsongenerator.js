cb70ebcd094bb3f2d964381c1628b72a805076fd
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-08-30 09:24:03 +1200

    Update jsongenerator.js

--- src/jsongenerator.js
+++ src/jsongenerator.js
@@ -1,4 +1,5 @@
 // This should be ran on any HTML archive version of a page on a thread. Nb generated code should be appended to existing JSON archive prior to the closing } bracket.
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var pages = eval(prompt('How many pages does this thread have?'));
 var parser = new DOMParser();
 var request = new XMLHttpRequest();
@@ -20,11 +21,12 @@ for(var item = 0; item < pages; item++){
 	if(request.status == 200){
 		posts = parser.parseFromString(request.responseText, 'text/html').querySelectorAll('.post');
 		for(var jtem = 0; jtem < posts.length; jtem++){
+			rd = posts[jtem].querySelector('.author').innerText.split('» ')[1].split(' ');
 			json.posts[json.posts.length] = {
 				posterID: posts[jtem].querySelectorAll('A')[1].attributes.href.value.split('=')[2],
 				posterName: posts[jtem].querySelector('.author').querySelector('Strong a').innerHTML,
 				postID: posts[jtem].querySelector('.author').querySelector('a').href.split('#')[1],
-				timestamp: posts[jtem].querySelector('.author').innerText.split('» ')[1],
+				timestamp: new Date(eval(rd.split(rd[3])), months.indexOf(rd[1]), eval(rd[2]).split(',')[0], eval(rd[4]).split(':')[0], eval(rd[4]).split(':')[1])
 				postHTML: posts[jtem].querySelector('.content').innerHTML
 			}
 		}
