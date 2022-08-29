17c5ca67c54511f77a612dc66b4d15916a4ab1a6
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-08-29 20:52:04 +1200

    Update jsongenerator.js

--- jsongenerator.js
+++ jsongenerator.js
@@ -16,10 +16,10 @@ for(var item = 0; item < pages; item++){
 	if(request.status == 200){
 		posts = parser.parseFromString(request.responseText, 'text/html').querySelectorAll('.post');
 		for(var jtem = 0; jtem < posts.length; jtem++){
-			json += '{posterID:"' + posts[jtem].querySelectorAll('A')[1].attributes.href.value.split('=')[2] + ','
-			+ 'posterName:"' + posts[jtem].querySelector('.author').querySelector('Strong a').innerHTML + ','
-			+ 'timestamp:"' + posts[jtem].querySelector('.author').innerText.split('» ')[1] + ','
-			+ 'postHTML:"' + posts[jtem].querySelector('.content').innerHTML + '},';
+			json += '{posterID:"' + posts[jtem].querySelectorAll('A')[1].attributes.href.value.split('=')[2] + '",'
+			+ 'posterName:"' + posts[jtem].querySelector('.author').querySelector('Strong a').innerHTML + '",'
+			+ 'timestamp:"' + posts[jtem].querySelector('.author').innerText.split('» ')[1] + '",'
+			+ 'postHTML:"' + posts[jtem].querySelector('.content').innerHTML + '"},';
 		}
 	}else{
 		json += ','
