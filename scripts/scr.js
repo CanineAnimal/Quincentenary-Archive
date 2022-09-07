171d928244254e004bcca2c2beab1d8d60c5a3ad
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-09-07 14:47:38 +1200

    Time may change me, but I can't trace time.

--- scripts/scr.js
+++ scripts/scr.js
@@ -10,7 +10,7 @@ document.querySelector('#JSON_LINK').onclick = function(){
 function searchLink(){
 	var link = document.querySelector('#LINK_SEARCH').value;
 	if(link.indexOf('forum.nationstates.net') == -1){
-		alert('URL not to "forum.nationstates.net". Please try again.')
+		alert('URL not to "forum.nationstates.net". Please try again.');
 	}else{
 		var request = new XMLHttpRequest();
 		request.open('GET', 'https://canineanimal.github.io/Quincentenary-Archive/pages/quincentenaryarchive.json', false);
@@ -28,25 +28,31 @@ function searchLink(){
 		}
 		if(!(link.indexOf('p=') == -1 && link.indexOf('#p') == -1)){
 			if(link.indexOf('#p') == -1){
-				postID = link.split('p=')[1].split('#p')[0]
+				postID = link.split('p=')[1].split('#p')[0];
 			}else{
-				postID = link.split('#p')[1]
+				postID = link.split('#p')[1];
 			}
 			for(var item = 0; item < jsonArray.length; item++){
 				for(var jtem = 0; jtem < jsonArray[item].posts.length; jtem++){
 					if(jsonArray[item].posts[jtem].postID.split('p')[1] == postID){
 						thread = jsonArray[item].shorthand;
+						page = Math.floor(jtem/25);
 					}
 				}
 			}
 		}
+		if(link.indexOf('start=') != -1){
+			page = Math.floor(eval(link.split('start=')[1].split('&')[0]));
+		}
 		if(window.thread == undefined){
-			alert('Thread not found. Either your link is incorrect, or the link you entered is from a thread not saved in the HTML Archive')
+			alert('Thread not found. Either your link is incorrect, or the link you entered is from a thread not saved in the HTML Archive');
 		}else{
-			if(window.postID == undefined){
-				location.href = 'https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + thread + '.html'
+			if(window.page == undefined){
+				location.href = 'https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + thread + '/0.html';
+			}else if(window.postID == undefined){
+				location.href = 'https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + thread + '/0.html';
 			}else{
-				location.href = 'https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + thread + '.html#p' + postID
+				location.href = 'https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + thread + '/' + page + '.html#p' + postID;
 			}
 		}
 	}
