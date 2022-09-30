dc70ab4bfc79c69425b5bf1518fbc103b2e7022b
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-09-30 20:09:07 +1300

    Just gonna have to be different man
    Every time I thought I'd got it made, it seemed the taste was not so sweet.

--- src/save.py
+++ src/save.py
@@ -1,21 +1,19 @@
-# Dependencies: PdfKit, WkHtmlToPdf, GitPython
+# Dependencies: PdfKit, WkHtmlToPdf
 # https://pypi.org/project/pdfkit/
 # https://wkhtmltopdf.org/
-# https://gitpython.readthedocs.io/en/stable/
-# Nb: this code is run in a clone of the QA-Archive repo.
+# Nb: this code is run in a folder wherein threads are stored as folders by this program.
+# The saved files are then uploaded manually to the 'QA-Archives' repository using the Github web interface.
 
 # Get modules
 import requests
 import pdfkit
 import time
-import git
 
 
 # Get inputs
 threadLink = input('Enter thread link: ')
 pageNo = int(input('Enter the amount of pages: '))
 tn = input('Enter shorthand version of thread name: ')
-repo = git.Repo('.git')
 
 # Loop through thread
 item = 0
@@ -30,15 +28,7 @@ while item < pageNo:
 		file = open(tn + '/' + str(item) + '.html', 'w', encoding='utf-8')
 		file.write(html)
 		file.close()
-		time.sleep(1)
-		print('Adding ' + str(item) + '.html...')
-		repo.index.add([tn + '/' + str(item) + '.html'])
-		print('Committing changes...')
-		repo.index.commit('Upload HTML Page')
-		print('Pushing changes...')
-		origin = repo.remote(name='origin')
-		origin.push()
-		print('Uploaded page ' + str(item + 1) + ' in HTML!')
+		print('Saved page ' + str(item + 1) + ' in HTML!')
 		if time.time() < originalTime + 6:
 			time.sleep(originalTime + 6 - time.time())
 
@@ -46,17 +36,9 @@ while item < pageNo:
 		print('Saving ' + str(item) + '.pdf...')
 		originalTime = time.time()
 		pdfkit.from_url(page, tn + '/' + str(item) + '.pdf', options={'custom-header':[('User-Agent','Script by The Ice States to save a Forum 7 thread.')]})
-		time.sleep(1)
-		print('Adding ' + str(item) + '.pdf...')
-		repo.index.add([tn + '/' + str(item) + '.pdf'])
-		print('Committing changes...')
-		repo.index.commit('Upload PDF Page')
-		print('Pushing changes...')
-		origin = repo.remote(name='origin')
-		origin.push()
-		print('Uploaded page ' + str(item + 1) + ' in PDF!')
+		print('Saved page ' + str(item + 1) + ' in PDF!')
 		if time.time() < originalTime + 6:
 			time.sleep(originalTime + 6 - time.time())
 		item += 1
 	except:
-		print('Unable to save page ' + str(item))
+		print('Unable to save page ' + str(item + 1) + '.')
