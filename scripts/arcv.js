16169cba7b96de80983784c5d55fb123cf594f06
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2022-10-03 10:18:21 +1300

    Removing Langolier Function

--- scripts/arcv.js
+++ scripts/arcv.js
@@ -1,13 +1,9 @@
 function accessArchive(num, ext){
 	if(ext == '.html'){
 		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
-	}else{
+	}else if(ext == '.pdf'){
 		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://cdn.jsdelivr.net/gh/CanineAnimal/QA-Archives@main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
 	}
-	document.querySelector('.LINKS').innerHTML = '<A HREF="https://canineanimal.github.io/Quincentenary-Archive/pages/">Back</A> | <A HREF="#" ONCLICK="closeIframe()">Close archive</A>';
-}
-function accessLangolierArchive(code){
-	document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://drive.google.com/file/d/' + code + '/preview" TITLE="Archive" WIDTH="600px" HEIGHT="600px" ALLOW="autoplay"/>'
 	document.querySelector('.LINKS').innerHTML = '<A HREF="https://canineanimal.github.io/Quincentenary-Archive/pages/">Back</A> | <A HREF="#" ONCLICK="closeIframe()">Close archive</A>';
 }
 function closeIframe(){
