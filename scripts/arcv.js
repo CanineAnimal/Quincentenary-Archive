575c78063ae57f38cf71d3c10fcef617f771cf07
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-01-25 19:19:54 +1300

    Update arcv.js

--- scripts/arcv.js
+++ scripts/arcv.js
@@ -2,7 +2,7 @@ function accessArchive(num, ext){
 	if(ext == '.html'){
 		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
 	}else if(ext == '.pdf'){
-		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://cdn.jsdelivr.net/gh/CanineAnimal/QA-Archives@main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
+		document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://cdn.jsdelivr.net/gh/CanineAnimal/QA-Archives/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + ext + '" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
 	}
 	document.querySelector('.LINKS').innerHTML = '<A HREF="https://canineanimal.github.io/Quincentenary-Archive/pages/">Back</A> | <A HREF="#" ONCLICK="closeIframe()">Close archive</A>';
 }
