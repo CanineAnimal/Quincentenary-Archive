565367e10aaae63b57accd8380a07e1b43332302
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-02-24 12:19:43 +1300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+console.log('%cWARNING', 'color:red; font-size:100px');
+console.log('%cInputting code into here may break the site, or possibly hack your computer! This feature is intended only for developers -- do NOT copy paste text here if you are not certain what you are doing.', 'font-size: 20px');
 document.querySelector('#HTML_LINK').onclick = function(){
 	location.href = 'https://canineanimal.github.io/Quincentenary-Archive/pages/html.html';
 }
