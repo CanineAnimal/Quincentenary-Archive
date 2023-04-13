4bbff7adb5cdf29208cbd29074f061279e11028c
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-04-14 11:13:01 +1200

    Update save.py
    -Made program cease trying to load a page after three failed attempts
    -Changed PDF generating code to rely on the already-downloaded HTML so as to reduce the amount of requests required
    -Made program automatically change URL for links between pages to use the archived pages instead of NS Forum link

--- src/save.py
+++ src/save.py
@@ -1,10 +1,14 @@
-# Dependencies: PdfKit, WkHtmlToPdf
+# Dependencies: PdfKit, WkHtmlToPdf, Lxml, Cssselect
+# https://cssselect.readthedocs.io/en/latest/
 # https://pypi.org/project/pdfkit/
 # https://wkhtmltopdf.org/
-# Nb: this code is run in a folder wherein threads are stored as folders by this program.
+# https://lxml.de/
+
+# NB: this code is run in a folder wherein threads are stored as folders by this program.
 # The saved files are then uploaded manually to the 'QA-Archives' repository using the Github web interface.
 
 # Get modules
+import lxml.html as hp
 import requests
 import pdfkit
 import time
@@ -20,35 +24,55 @@ if not os.path.exists('./' + tn):
     os.makedirs('./' + tn)
 
 # Loop through thread
+tries = 0
 item = 0
 originalTime = time.time()
 while item < pageNo:
-	try:
-		# Save page HTML
+	try:	
+		# Fetch page HTML
 		print('Delay since last request: ' + str(time.time() - originalTime) + ' s. Fetching ' + str(item) + '.html...')
 		page = threadLink + '&start=' + str(item * 25)
-		html = requests.get(page, headers={'User-Agent': 'Script by The Ice States to save a Forum 7 thread.'}).text.replace('href="./', 'href="https://forum.nationstates.net/').replace('src="./', 'href="https://forum.nationstates.net/').replace('src="//', 'src="https://').replace('href="//', 'href="https://')
-		print('Saving ' + str(item ) + '.html...')
+		html = requests.get(page, headers={'User-Agent': 'Script by The Ice States to save a Forum 7 thread.'}).text
 		originalTime = time.time()
-		file = open(tn + '/' + str(item) + '.html', 'w', encoding='utf-8')
+
+		# Fix links to NS website and replace links to forum pages
+		html = html.replace('href="./', 'href="https://forum.nationstates.net/').replace('src="./', 'href="https://forum.nationstates.net/').replace('src="//', 'src="https://').replace('href="//', 'href="https://')
+		dom = hp.fromstring(html)
+		for link in dom.cssselect('.pagination span:nth-of-type(1) a'):
+			link.set('href', value=('https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + tn + '/' + str(int(link.text_content()) - 1) + '.html'))
+		html = hp.tostring(dom);
+		
+		# Save page HTML
+		print('Saving ' + str(item) + '.html...')
+		file = open(tn + '/' + str(item) + '.html', 'wb')
 		file.write(html)
 		file.close()
 		print('Saved page ' + str(item + 1) + ' in HTML!')
 
+		# Replace links to other HTML pages with PDF pages
+		dom = hp.fromstring(html)
+		for link in dom.cssselect('.pagination span:nth-of-type(1) a'):
+			link.set('href', value=('https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + tn + '/' + str(int(link.text_content()) - 1) + '.pdf'))
+		html = hp.tostring(dom);
+		
+		# Save page PDF
+		print('Saving ' + str(item) + '.pdf...')
+		pdfkit.from_string(html.decode('utf-8'), tn + '/' + str(item) + '.pdf', {'enable-local-file-access': ''})
+		originalTime = time.time()
+		print('Saved page ' + str(item + 1) + ' in PDF!')
+		
 		# Wait to avoid violating NS rate limits
 		if time.time() < originalTime + 20:
 			time.sleep(originalTime + 20 - time.time())
 		
-		# Save page PDF
-		print('Delay since last request: ' + str(time.time() - originalTime) + ' s. Saving ' + str(item) + '.pdf...')
-		pdfkit.from_url(page, tn + '/' + str(item) + '.pdf', options={'custom-header':[('User-Agent','Script by The Ice States to save a Forum 7 thread.')]})
-		originalTime = time.time()
-		print('Saved page ' + str(item + 1) + ' in PDF!')
-		
-		# Second rate limit wait
-		if time.time() < originalTime + 20:
-			time.sleep(originalTime + 20 - time.time())
-		
+		tries = 0
 		item += 1
 	except:
-		print('Unable to save page ' + str(item + 1) + '.')
+		# Move on to next page after three attempts
+		tries += 1
+		if(tries == 3):
+			item += 1
+			tries = 0
+			print('Unable to save page ' + str(item + 1) + '.')
+		else:
+			print('Unable to save page ' + str(item + 1) + '. Trying again...')
