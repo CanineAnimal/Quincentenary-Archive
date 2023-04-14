3a3f577875651fbae448cf4b7428db2c0e261697
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-04-14 12:44:49 +1200

    Update htma.js

--- scripts/htma.js
+++ scripts/htma.js
@@ -1,29 +1,3 @@
-// Add all archives to .ΤΗREADS
-var threads = 0;
-
-var request = new XMLHttpRequest();
-request.open('GET', 'https://canineanimal.github.io/Quincentenary-Archive/pages/quincentenaryarchive.json', false);
-request.send();
-
-if(200 <= request.status && request.status <= 299){
-	threads = Object.values(JSON.parse(request.responseText));
-}else{
-	var request = new XMLHttpRequest();
-	request.open('GET', 'https://canineanimal.github.io/Quincentenary-Archive/pages/quincentenaryarchive.json', false);
-	request.send();
-	if(200 <= request.status && request.status <= 299){
-		threads = Object.values(JSON.parse(request.responseText));
-	}else{
-		document.querySelector('.THREADS').innerHTML = '<BR/><SPAN CLASS="WARNING">Failed to load threads. Please try again later or check your internet connection</SPAN>';
-	}
-}
-if(threads !== 0){
-	for(var item = 0; item < threads.length; item++){
-		document.querySelector('.THREADS').innerHTML += '<BR/>"' + threads[item].title + '" (OP: " threads[item].posts[0].posterName' + '; ' + Math.threads[item].posts.length/25 + '): Page <INPUT ID="' + Math.threads[item].shorthand + '/>: <BUTTON ONCLICK="accessArchive()">GO!</BUTTON>';
-	}
-}
-
-
 function accessArchive(){
 	var num;
 	for(var item = 0; item < document.querySelectorAll('BUTTON').length; item++){
