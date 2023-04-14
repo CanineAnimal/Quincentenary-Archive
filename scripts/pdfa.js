ee1ae33f62eedcb52672361ce4c2e55f636d69fe
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-04-14 13:12:42 +1200

    Update pdfa.js

--- scripts/pdfa.js
+++ scripts/pdfa.js
@@ -6,6 +6,6 @@ function accessArchive(){
 			break;
 		}
 	}
-	document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://htmlpreview.github.io/?https://github.com/CanineAnimal/QA-Archives/blob/main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + '.pdf" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
+	document.querySelector('#IFRAME_SPACE').innerHTML = '<IFRAME SRC="https://raw.githubusercontent.com/CanineAnimal/QA-Archives/main/' + document.querySelectorAll('INPUT')[num].id + '/' + (eval(document.querySelectorAll('INPUT')[num].value) - 1) + '.pdf" TITLE="Archive" WIDTH="600px" HEIGHT="600px"/>';
 	document.querySelector('.LINKS').innerHTML = '<A HREF="https://canineanimal.github.io/Quincentenary-Archive/pages/">Back</A> | <A HREF="#" ONCLICK="closeIframe()">Close archive</A>';
 }
