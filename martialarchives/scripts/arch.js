33b78505a307c05299d472ea24e400942e181c1a
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-06-20 19:45:16 +1200

    Add Web Components

--- martialarchives/scripts/arch.js
+++ martialarchives/scripts/arch.js
@@ -2,9 +2,23 @@
 console.log('%cWARNING', 'color:red; font-size:100px');
 console.log('%cInputting code into here may break the site, or possibly hack your computer! This feature is intended only for developers -- do NOT copy paste text here if you are not certain what you are doing.', 'font-size: 20px');
 
+// Define Web Components
+customElements.define('THREAD', class extends HTMLElement{
+	constructor(){
+		super();
+		var tmpt = document.querySelector('#THREAD').content;
+		this.attachShadow({mode: 'open'}).appendChild(tmpt.cloneNode(true));
+	}
+});
+customElements.define('BUT', class extends HTMLElement{
+	constructor(){
+		super();
+		var tmpt2 = document.querySelector('#BUT').content;
+		this.attachShadow({mode: 'open'}).appendChild(tmpt2.cloneNode(true));
+	}
+})
 // Add all archives to .ΤΗREADS
 var threads = 0;
-
 var request = new XMLHttpRequest();
 request.open('GET', 'https://canineanimal.github.io/Quincentenary-Archive/martialarchives/pages/martialrepositories.json', false);
 try{request.send()}catch(e){};
@@ -23,7 +37,7 @@ if(200 <= request.status && request.status <= 299){
 if(threads !== 0){
 	document.querySelector('.THREADS').innerHTML = '';
 	for(var item = 0; item < threads.length; item++){
-		document.querySelector('.THREADS').innerHTML += '<BR/>"' + threads[item].title + '" (OP: ' + threads[item].posts[0].posterName + '; ' + Math.ceil(threads[item].posts.length/25) + ' pages): Page <INPUT ID="' + threads[item].shorthand + '"/>: <BUTTON ONCLICK="accessArchive()">GO!</BUTTON>';
+		document.querySelector('.THREADS').innerHTML += '<THREAD><SPAN SLOT="THREADNAME">' + threads[item].title + '</SPAN><SPAN SLOT="OP">' + threads[item].posts[0].posterName + '</SPAN><SPAN SLOT="PAGES">' + Math.ceil(threads[item].posts.length/25) + '</SPAN></THREAD><INPUT ID="' + threads[item].shorthand + '"/><BUT></BUT>';
 	}
 }
 
