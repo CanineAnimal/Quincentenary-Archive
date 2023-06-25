6159bf40069ab931fed0361f81d4c0a927c7e35e
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-06-25 13:09:24 +1200

    Update arch.js

--- scripts/arch.js
+++ scripts/arch.js
@@ -13,26 +13,28 @@ customElements.define('thd-tg' /* Stupid hyphen requirement */, class extends HT
 });
 
 // Add all archives to .ΤΗREADS
-var threads = 0;
-var request = new XMLHttpRequest();
-request.open('GET', 'https://canineanimal.github.io/Quincentenary-Archive/pages/quincentenaryarchive.json', false);
-try{request.send()}catch(e){};
-if(200 <= request.status && request.status <= 299){
-	threads = Object.values(JSON.parse(request.responseText));
-}else{
+function loadThreads(url){
+	var threads = 0;
 	var request = new XMLHttpRequest();
-	request.open('GET', 'https://canineanimal.github.io/Quincentenary-Archive/pages/quincentenaryarchive.json', false);
+	request.open('GET', url, false);
 	try{request.send()}catch(e){};
 	if(200 <= request.status && request.status <= 299){
 		threads = Object.values(JSON.parse(request.responseText));
 	}else{
-		document.querySelector('.THREADS').innerHTML = '<BR/><SPAN CLASS="WARNING">Failed to load threads. Please try again later or check your internet connection</SPAN>';
+		var request = new XMLHttpRequest();
+		request.open('GET', url, false);
+		try{request.send()}catch(e){};
+		if(200 <= request.status && request.status <= 299){
+			threads = Object.values(JSON.parse(request.responseText));
+		}else{
+			document.querySelector('.THREADS').innerHTML = '<BR/><SPAN CLASS="WARNING">Failed to load threads. Please try again later or check your internet connection</SPAN>';
+		}
 	}
-}
-if(threads !== 0){
-	document.querySelector('.THREADS').innerHTML = '';
-	for(var item = 0; item < threads.length; item++){
-		document.querySelector('.THREADS').innerHTML += '<THD-TG><SPAN SLOT="THREAD_NAME">' + threads[item].title + '</SPAN><SPAN SLOT="OP">' + threads[item].posts[0].posterName + '</SPAN><SPAN SLOT="PAGES">' + Math.ceil(threads[item].posts.length/25) + '</SPAN></THD-TG><INPUT ID="' + threads[item].shorthand + '"/>: <BUTTON ONCLICK="accessArchive()">GO!</BUTTON>';
+	if(threads !== 0){
+		document.querySelector('.THREADS').innerHTML = '';
+		for(var item = 0; item < threads.length; item++){
+			document.querySelector('.THREADS').innerHTML += '<THD-TG><SPAN SLOT="THREAD_NAME">' + threads[item].title + '</SPAN><SPAN SLOT="OP">' + threads[item].posts[0].posterName + '</SPAN><SPAN SLOT="PAGES">' + Math.ceil(threads[item].posts.length/25) + '</SPAN></THD-TG><INPUT ID="' + threads[item].shorthand + '"/>: <BUTTON ONCLICK="accessArchive()">GO!</BUTTON>';
+		}
 	}
 }
 
